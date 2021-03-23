f730a5019cf475100bda4ae0b1015b0ebcfa72e9
wook95 <wook@iugchang-ui-MacBookPro.local>
2021-03-23 14:49:42 +0900

    memberJoin

--- src/main/java/com/arc/s3/member/MemberDAO.java
+++ src/main/java/com/arc/s3/member/MemberDAO.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
@@ -13,7 +14,7 @@ import org.springframework.stereotype.Repository;
 public class MemberDAO {
 
 	
-
+	@Autowired
 	private SqlSession sqlSession;
 	private String NAMESPACE = "com.arc.s3.member.MemberDAO";
 	
--- src/test/java/com/arc/s3/memberTest.java
+++ src/test/java/com/arc/s3/memberTest.java
@@ -1,17 +1,19 @@
 package com.arc.s3;
 
-import static org.junit.Assert.*;
+
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.arc.s3.member.MemberDAO;
 import com.arc.s3.member.MemberDTO;
 
 public class memberTest extends MyAbstractTest{
 
-
+	@Autowired
 	private MemberDAO memberDAO;
 	
+	@Test
 	public void memberJoinTest() throws Exception{
 		
 		MemberDTO memberDTO = new MemberDTO();
