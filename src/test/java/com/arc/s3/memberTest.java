c8dec2eb452e775ba56a9358280ea81cce0ebb19
wook95 <wook@iugchang-ui-MacBookPro.local>
2021-03-23 14:19:27 +0900

    2

---
+++ src/test/java/com/arc/s3/memberTest.java
@@ -0,0 +1,14 @@
+package com.arc.s3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class memberTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
